2109f749ced633b1f3b425990db103cc444d3a02
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-11-22 14:31:48 +0530

    Add files via upload

--- src/Components/DesktopComponent.js
+++ src/Components/DesktopComponent.js
@@ -35,7 +35,7 @@ function DesktopComponent() {
           background:
             "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
         }}
-        position="sticky"
+        position="fixed"
       >
         <Container maxWidth="xl">
           <Toolbar disableGutters>
