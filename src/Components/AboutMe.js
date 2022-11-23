134cc8a788d248fd015d53ab90e56d278e020acb
fbpatel003 <fenilbabariya.fb@gmail.com>
2022-11-23 17:17:09 +0530

    517

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,6 +3,7 @@ import "./AboutMeStyle.css";
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
 import IMG from "../Images/IMG_4143.jpg";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function AboutMe(props) {
   return (
@@ -20,14 +21,43 @@ function AboutMe(props) {
               An Engineer graduated with Electricals' Degree, More than three
               years of experience in the industry, Involved in Team management
               and Project Management.
-              <br /> <br />
-              I have serious passion for UI effects, animations and creating intuitive,
-              dynamic user experiences. 
+              <br /> <br />I have serious passion for UI effects, animations and
+              creating intuitive, dynamic user experiences.
               <span className="Blue"> Let's make something special. </span>
             </div>
           </Grid>
         </Grid>
       </Box>
+      <Grid
+        className="wrapper"
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="flex-start"
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        <Grid className="icon facebook" xs={4}>
+          <span className="tooltip">Facebook</span>
+          <span>
+            <i className="fab fa-facebook-f" />
+          </span>
+        </Grid>
+        <Grid className="icon github" xs={4}>
+          <div href="https://github.com">
+          <span className="tooltip">Github</span>
+          <span>
+            <GitHubIcon sx={{ fontSize: 40 }} />
+          </span>
+          </div>
+        </Grid>
+        <Grid className="icon instagram" xs={4}>
+          <span className="tooltip">Instagram</span>
+          <span>
+            <i className="fab fa-instagram" />
+          </span>
+        </Grid>
+      </Grid>
     </>
   );
 }
