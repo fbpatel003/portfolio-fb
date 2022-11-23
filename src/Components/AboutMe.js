3abda33f80eff74db5efd3b8541bf9389faf5d4c
fbpatel003 <fenilbabariya.fb@gmail.com>
2022-11-23 12:36:41 +0530

    1236

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./AboutMeStyle.css";
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
+import SpeedRoundedIcon from '@mui/icons-material/SpeedRounded';
+import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded';
+import EmojiObjectsRoundedIcon from '@mui/icons-material/EmojiObjectsRounded';
+import RocketLaunchRoundedIcon from '@mui/icons-material/RocketLaunchRounded';
 
 function AboutMe() {
   return (
@@ -10,16 +14,25 @@ function AboutMe() {
       <Box className="responsiveBoxSpeciality" sx={{ width: "98%" }}>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           <Grid xs={6} md={3}>
-            <div className="specialityBox"></div>
+            <div className="specialityBox">
+                <SpeedRoundedIcon className="specialityIcon" sx={{ fontSize: 80 }} />
+                <div className="SpecialityName"> Fast </div>
+            </div>
           </Grid>
           <Grid xs={6} md={3}>
-            <div className="specialityBox"></div>
+            <div className="specialityBox">
+                <DevicesRoundedIcon className="specialityIcon" sx={{ fontSize: 80 }} />
+            </div>
           </Grid>
           <Grid xs={6} md={3}>
-            <div className="specialityBox"></div>
+            <div className="specialityBox">
+            <EmojiObjectsRoundedIcon className="specialityIcon" sx={{ fontSize: 80 }} />
+            </div>
           </Grid>
           <Grid xs={6} md={3}>
-            <div className="specialityBox"></div>
+            <div className="specialityBox">
+            <RocketLaunchRoundedIcon className="specialityIcon" sx={{ fontSize: 80 }} />
+            </div>
           </Grid>
         </Grid>
       </Box>
