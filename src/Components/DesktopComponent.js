3cafe4c9121948adce1227826a30c128ea557a5b
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-11-25 16:20:12 +0530

    Add files via upload

--- src/Components/DesktopComponent.js
+++ src/Components/DesktopComponent.js
@@ -36,8 +36,9 @@ function DesktopComponent() {
       <AppBar
         sx={{
           background:
-            "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
-        }}
+            // "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+            "linear-gradient(0deg, rgba(62,62,62,1) 0%, rgba(195,195,195,1) 100%)"
+          }}
         position="fixed"
       >
         <Container maxWidth="xl">
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React from "react";
 import "./PortfolioStyle.css";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
 
 function Portfolio() {
-    return ( 
-        <>
-        <h1 className="TitleAboutMe">Portfolio</h1>
-        </>
-     );
+  return (
+    <>
+      <h1 className="TitleAboutMe">Portfolio</h1>
+
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={3}>
+            <div className="card">
+              <div className="card__side card__side--front-1">
+                <img
+                  className="ProjectImg"
+                  src={require("../Images/IMAGE.jpg")}
+                  alt=""
+                />
+                <div className="ProjectTitle">This Website Itself</div>
+                <p className="smallData">
+                  A personal website with all the resume data, skills, contact
+                  details and portfolio of all projects. created using ReactJs
+                  and MUI css framework.
+                </p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <ul class="KeyPointslist">
+                  <li className="ListPoint" >Responsive</li>
+                  <li className="ListPoint" >Well Designed</li>
+                  <li className="ListPoint" >Fast Loading</li>
+                  <li className="ListPoint">structured components </li>
+                  <li className="ListPoint">CSS: MUI</li>
+                  <li className="ListPoint" >Optimized for all devices</li>
+                </ul>
+                <a href="#popup" className="btn btn--white">
+                  Visit &nbsp;
+                  <OpenInNewRoundedIcon fontSize="small" />
+                </a>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
 export default Portfolio;
